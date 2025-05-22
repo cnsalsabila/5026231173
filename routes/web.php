3e237e9bf45de089f8531a66f628c7ffeb80dd9e
Naura Salsabila <naurakcrt@gmail.com>
2025-05-22 15:48:51 +0700

    Pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-// import java.io ;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
 
 //System.out.println
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('dosen', [DosenController::class, 'index']);
+Route::get('welcome', [DosenController::class, 'welcome']);
+
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+// route blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+
 Route::get('halo', function () {
 	return "<h1>Halo, Selamat datang di tutorial laravel www.malasngoding.com</h1>";
 });
 
-Route::get('blog', function () {
-	return view('blog');
-});
+// Route::get('blog', function () {
+// 	return view('blog');
+// });
 
 Route::get('pertama', function () {
 	return view('pertama');
